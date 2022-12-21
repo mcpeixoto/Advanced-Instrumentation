484a38eafaf1d9f9128a142d762bbef32487aca8
Miguel Caçador Peixoto <miguelpeixoto457@gmail.com>
2022-12-21 23:11:32 +0000

    removed unused variables

--- desespera/main.c
+++ desespera/main.c
@@ -1,61 +1,28 @@
-/*
- * THIS PROJECT IS AN EXAMPLE OF SEVERAL FUNCTIONALITIES OF THE MICROCONTROLLER AND
- * OF HOW TO USE THEM AND ESTABLISH A CONNECTION BETWEEN FUNCTIONS WRITEN IN C AND ASM
- * 
- * THE CONFIGURATION OF ALL PERIPHERALS IS DONE IN THE CONFIG.ASM FILE. 
- * 
- * THE FUNCTION PASS_VARIABLE_BETWEEN_C_AND_ASM IS WRITTEN IN ASM AND IT IS AN EXAMPLE OF HOW 
- * TO HANDLE VARIABLES IN BOTH C AND ASM. THE INPUT ARGUMENT OF THE FUNCTION IN C (IN THIS CASE, 
- * THE NUMBER 3), IS STORED IN THE WORKING REGISTER W WHEN THE ASM FUNCTION IS CALLED. THE OUTPUT
- * OF THE FUNCTION RECEIVED IN THE C CALL OF THE FUNCTION IS WHATEVER VALUE IS STORED IN W WHEN 
- * THE RETURN HAPPENS IN THE ASM FUNCTION.
- * IN THIS EXAMPLE, THE FUNCTION RECEIVES THE INPUT VALUE (3), ADDS (5), RETURNS THE RESULT AND
- * ALSO ADDS 1 TO THE GLOBAL VARIABLE var_global.
- * 
- * AFTER THAT WE GO TO AN INFINITE LOOP WHERE EVERY 2 SECONDS WE PRINT THE GLOBAL VARIABLE AND
- * THE RESULT OF THE (3+5) OPERATION
- * 
- * APPROXIMATELY EVERY 500 MS, THE TIMER0 INTERRUPT IS TRIGGERED AND TOGGLES PORTA,5
- * ADDITIONALLY, AN ADC CONVERSION IS TRIGGERED. WHEN IT IS COMPLETED, ANOTHER INT HAPPENS WHICH
- * SENDS THE CONVERTED VALUE VIA UART.
- * 
- * WHEN THE UART RECEIVES A VALUE, IT TRIGGERS AN INTERRUPT. WE GO TO INT_HANDLER, WHICH THEN CALLS
- * FUNCTION GETCH (A FUNCTION WRITTEN IN C) THAT READS AND STORES THE RECEIVED VALUE IN VARIABLE  get_char. 
- * IN THE INFINITE LOOP, WHENEVER THE VALUE OF GET_CHAR IS NOT 0, IT PRINTS GET_CHAR+1.
- */
-
-
 #include <xc.h>
 #include <stdio.h>
 #include <float.h>
 #include <string.h>
 #include "TEDS_definition.h"
 
-#define _XTAL_FREQ 32000000   //the clock frequency defined in config() must be provided here for the delay function
+// The clock frequency defined in config() must be provided here for the delay function
+#define _XTAL_FREQ 32000000
 
-//declaration of global variables and functions
+// Declaration of global variables and functions
 void config(void);
 void __interrupt() int_handler(void); 
-
-
 void define_METATEDS(void);
 void define_TCTEDS(void);
 void Identify_NCAP_cmd(void);
 void send_METATEDS(void);
 void send_TCTEDS(uint8_t);
 uint8_t get_char (void);
-void fail (void);
+void fail(void);
 void write_success(void);
 void send_values (uint8_t);
-void enviar_valor(uint8_t canal);
-char pos;
-char var_global;
 uint8_t info_ind = 0;   // indice do proximo array
 uint8_t info[6];        // array que vai receber primeiros 6 bits da NCAP
 uint8_t value_ind= 0;   // indice do proximo array
 uint8_t value[256];     // array que recebe offset e valores da NCAP
-void echo (void);       // fun��o de teste da comunica��o 
-uint8_t test;           // variavel de teste 
 
 //create the structs for the Meta TEDS and for the TransducerChannels TEDS for 3 channels
 struct METATEDS_TEMPLATE METATED;
@@ -194,38 +161,6 @@ void write_success(void){   // codigo de sucesso
     }
     return;
 }
- //void enviar_valor(uint8_t canal){
- //     
- //     putch(1);
- //     putch(0);      // mensagem de sucesso
-//      putch(1);
-      
- 
-//      if(canal==1) {
-//        ADPCH = 0B00000001 ;        // O input do ADC � o pin RA1 para o valor do x
-//        ADCON0bits.ADGO = 1 ;       // fazer uma convers�o
-//        while(PIR1bits.ADIF == 0){
-            ;
-//        }
-//        putch(ADRESL);              // mandar valor 
-//        PIR1bits.ADIF = 0;          // reset da flag do ADC
-//      }
-//      if(canal==2) {
-//        ADPCH = 0B00000010 ;        // O input do ADC � o pin RA2 para o valor do y
-//        ADCON0bits.ADGO = 1 ;       // fazer uma convers�o
-//        while(PIR1bits.ADIF == 0){
-//            ;
-//        }
-//        putch(ADRESL);              // mandar valor
-//        PIR1bits.ADIF = 0;          // reset da flag do AD
-//      }
-//      if(canal==3) {
- //       putch(DAC1CON1);            // se quiser ler do transducer 3 escrever o valor do DAC 
-//      }
-      
-// }
-
-
 
 
 
